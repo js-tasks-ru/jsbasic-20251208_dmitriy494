b65896b46bad0e268889cdb088f2b2c326f92af4
Dmitry Strelnikov <oddff@mail.ru>
2025-12-11 21:28:18 +0300

    вычисление факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    return 1;
+  } else if (typeof n === "number") {
+    let result = n;
+
+    for (let i = --n; i > 0; i--) {
+      result *= i;
+    }
+
+    return result;
+  } else {
+    return "Не число";
+  }
 }
