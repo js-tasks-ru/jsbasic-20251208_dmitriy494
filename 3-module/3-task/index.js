ff924c7b4cfe81d05aff293ea98848d8c817bc8e
Dmitry Strelnikov <oddff@mail.ru>
2025-12-22 20:57:32 +0300

    Перевод текста

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("-");
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+  }
+
+  return arr.join("");
 }
