6840db62f5fae1d2ee1ba957229474e356f6d854
Dmitry Strelnikov <oddff@mail.ru>
2025-12-26 00:57:51 +0300

    Функция highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,39 @@
 function highlight(table) {
-  // ваш код...
+  let Age;
+  let Gender;
+  let Status;
+  for (let el of table.rows[0].cells) {
+    switch (el.innerText) {
+      case "Age":
+        Age = el.cellIndex;
+        break;
+      case "Gender":
+        Gender = el.cellIndex;
+        break;
+      case "Status":
+        Status = el.cellIndex;
+        break;
+    }
+  }
+
+  for (const row of table.tBodies[0].rows) {
+    console.log(row.cells);
+    let cellAge = row.cells[Age];
+    let cellGender = row.cells[Gender];
+    let cellStatus = row.cells[Status];
+
+    if (Number(cellAge.textContent) < 18)
+      row.style.textDecoration = "line-through";
+
+    row.classList.add(cellGender.textContent === "m" ? "male" : "female");
+
+    const available = cellStatus.dataset.available;
+
+    if (!available) {
+      row.setAttribute("hidden", "");
+      continue;
+    }
+
+    row.classList.add(available === "true" ? "available" : "unavailable");
+  }
 }
